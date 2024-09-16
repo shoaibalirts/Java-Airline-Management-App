f954064e0a7f0ecac3e60f3f43d54bdac7dc47b7
shoaibalirts <shoaibali.rts@gmail.com>
2024-09-16 17:08:51 +0200

    Analyzed the difference between Singleton and Stateful EJBs. Singleton shares data across Servlets (entire web application) whereas Stateful preserves the state per Servlet

---
+++ src/com/airline/controllers/AddOne.java
@@ -0,0 +1,67 @@
+package com.airline.controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.airline.service.CounterBean;
+import com.airline.service.CounterStatefulBean;
+
+/**
+ * Servlet implementation class AddOne
+ */
+@WebServlet("/AddOne")
+public class AddOne extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@EJB
+	private CounterBean cb;
+	@EJB
+	private CounterStatefulBean cbStateful;
+	
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddOne() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		out.println("The current value for the SINGLETON Bean is: "+cb.getCount());
+		out.println("The current value for the STATEFUL Bean is: "+cbStateful.getCount());
+
+		cb.addOneToCount();
+		cbStateful.addOneToCount();
+		
+
+		out.println();
+		out.println();
+		out.println();
+		
+		out.println("The current value for the SINGLETON Bean is: "+cb.getCount());
+		out.println("The current value for the STATEFUL Bean is: "+cbStateful.getCount());
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
